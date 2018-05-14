a0941b4499f05d55a14437fc89d9f8707c0eecbe
Informatica Para Terceira Idade <i3i@proway.treina>
2018-05-14 17:08:17 -0300

    Exercicio 04 utilizando WindowsForm

---
+++ ExercicioWFA/ExercicioWFA/Exercicio04.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ExercicioWFA
+{
+    partial class Exercicio04
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.txtTextao = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite um número para tabuada";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(378, 41);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(97, 20);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.BackColor = System.Drawing.Color.Black;
+            this.btnGerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGerar.ForeColor = System.Drawing.Color.White;
+            this.btnGerar.Location = new System.Drawing.Point(378, 88);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(97, 54);
+            this.btnGerar.TabIndex = 2;
+            this.btnGerar.Text = "Gerar";
+            this.btnGerar.UseVisualStyleBackColor = false;
+            this.btnGerar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtTextao
+            // 
+            this.txtTextao.BackColor = System.Drawing.Color.LavenderBlush;
+            this.txtTextao.Location = new System.Drawing.Point(12, 232);
+            this.txtTextao.Multiline = true;
+            this.txtTextao.Name = "txtTextao";
+            this.txtTextao.Size = new System.Drawing.Size(111, 213);
+            this.txtTextao.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("News701 BT", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(12, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Resultado";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Exercicio04
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(547, 457);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTextao);
+            this.Controls.Add(this.btnGerar);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label1);
+            this.Name = "Exercicio04";
+            this.Text = "Exercicio04";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.TextBox txtTextao;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ ExercicioWFA/ExercicioWFA/Exercicio04.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExercicioWFA
+{
+    public partial class Exercicio04 : Form
+    {
+        public Exercicio04()
+        {
+            InitializeComponent();
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+                          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numero = Convert.ToInt32(txtNumero.Text);
+
+            int tabuada0 = numero * 0;
+            int tabuada1 = numero * 1;
+            int tabuada2 = numero * 2;
+            int tabuada3 = numero * 3;
+            int tabuada4 = numero * 4;
+            int tabuada5 = numero * 5;
+            int tabuada6 = numero * 6;
+            int tabuada7 = numero * 7;
+            int tabuada8 = numero * 8;
+            int tabuada9 = numero * 9;
+            int tabuada10 = numero * 10;
+
+            string textao = string.Format(numero + " X " + " 0 " + " = " + tabuada0);
+            textao += string.Format("\r\n" + numero + " X " + " 1 " + " = " + tabuada1);
+            textao += string.Format("\r\n" + numero + " X " + " 2 " + " = " + tabuada2);
+            textao += string.Format("\r\n" + numero + " X " + " 3 " + " = " + tabuada3);
+            textao += string.Format("\r\n" + numero + " X " + " 4 " + " = " + tabuada4);
+            textao += string.Format("\r\n" + numero + " X " + " 5 " + " = " + tabuada5);
+            textao += string.Format("\r\n" + numero + " X " + " 6 " + " = " + tabuada6);
+            textao += string.Format("\r\n" + numero + " X " + " 7 " + " = " + tabuada7);
+            textao += string.Format("\r\n" + numero + " X " + " 8 " + " = " + tabuada8);
+            textao += string.Format("\r\n" + numero + " X " + " 9 " + " = " + tabuada9);
+            textao += string.Format("\r\n" + numero + " X " + " 10 " + " = " + tabuada10);
+
+            txtTextao.Text = textao;
+
+        }
+    }
+}
