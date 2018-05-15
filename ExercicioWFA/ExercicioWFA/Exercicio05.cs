bd056be9fbbc2520841d535746d2418999f12815
Marcos Luiz Mittag Filho <Marquinhoszx_xd@Outlook.com>
2018-05-15 14:51:04 -0300

    Exercicio 05 utilizando WindowsForm

--- ExercicioWFA/ExercicioWFA/Exercicio05.cs
+++ ExercicioWFA/ExercicioWFA/Exercicio05.cs
@@ -22,11 +22,13 @@ namespace ExercicioWFA
             string nomeCarro = txtNomeCarro.Text;
             double valorDoCarro = Convert.ToDouble(mtbValorCarro.Text);
             double valorDaParcela = Convert.ToDouble(mtbValorParcela.Text);
-            int quantidadeParcelas = Convert.ToInt32(txtQuantidadeMeses);
+            int quantidadeParcelas = Convert.ToInt32(txtQuantidadeMeses.Text);
             double valorTotalFinancimaneto = valorDaParcela * quantidadeParcelas;
-        {
+            double diferencaPreco = valorDoCarro - valorTotalFinancimaneto;
 
-            }
+            string textao = string.Format("Nome do carro: {0}\r\nValor do carro: {1:n}\r\nValor da Parcela: {2:n}\r\nQuantidade de parcelas: {3:n}\r\nValor do financiamento: {4:}\r\nDiferença entre valor do financiamento {5:n}", nomeCarro, valorDoCarro, valorDaParcela, quantidadeParcelas, valorTotalFinancimaneto, diferencaPreco);  
+            
+            txtTextao.Text = textao;
         }
     }
 }
