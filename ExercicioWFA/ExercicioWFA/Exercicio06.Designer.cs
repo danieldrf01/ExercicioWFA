6586e80c196cd006ad1eac497051846f76fa1984
Marcos Luiz Mittag Filho <Marquinhoszx_xd@Outlook.com>
2018-05-15 16:21:53 -0300

    Exercicio 06 utilizando WindowsForm

---
+++ ExercicioWFA/ExercicioWFA/Exercicio06.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace ExercicioWFA
+{
+    partial class Exercicio06
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTextao = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.txtNota4 = new System.Windows.Forms.TextBox();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nota01";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(132, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nota02";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Window;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(228, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 21);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nota03";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Window;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(341, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 21);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nota04";
+            // 
+            // txtTextao
+            // 
+            this.txtTextao.Location = new System.Drawing.Point(345, 296);
+            this.txtTextao.Multiline = true;
+            this.txtTextao.Name = "txtTextao";
+            this.txtTextao.Size = new System.Drawing.Size(177, 114);
+            this.txtTextao.TabIndex = 4;
+            this.txtTextao.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.label5.Font = new System.Drawing.Font("Segoe WP", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(385, 256);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Resultado";
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(42, 76);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(64, 20);
+            this.txtNota1.TabIndex = 1;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(136, 76);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(64, 20);
+            this.txtNota2.TabIndex = 2;
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(232, 76);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(64, 20);
+            this.txtNota3.TabIndex = 3;
+            // 
+            // txtNota4
+            // 
+            this.txtNota4.Location = new System.Drawing.Point(345, 76);
+            this.txtNota4.Name = "txtNota4";
+            this.txtNota4.Size = new System.Drawing.Size(64, 20);
+            this.txtNota4.TabIndex = 4;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Font = new System.Drawing.Font("Segoe Print", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecutar.Location = new System.Drawing.Point(74, 241);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(126, 55);
+            this.btnExecutar.TabIndex = 9;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Exercicio06
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(683, 441);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.txtNota4);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTextao);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Exercicio06";
+            this.Text = "Exercicio06";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTextao;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.TextBox txtNota4;
+        private System.Windows.Forms.Button btnExecutar;
+    }
+}
---
+++ ExercicioWFA/ExercicioWFA/Exercicio06.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExercicioWFA
+{
+    public partial class Exercicio06 : Form
+    {
+        public Exercicio06()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double numero1 = Convert.ToDouble(txtNota1.Text);
+            double numero2 = Convert.ToDouble(txtNota2.Text);
+            double numero3 = Convert.ToDouble(txtNota3.Text);
+            double numero4 = Convert.ToDouble(txtNota4.Text);
+            double media = (numero1 + numero2 + numero3 + numero4) / 4;
+            
+            try
+            {
+                 numero1 = Convert.ToDouble(txtNota1.Text);
+                 numero2 = Convert.ToDouble(txtNota2.Text);
+                 numero3 = Convert.ToDouble(txtNota3.Text);
+                 numero4 = Convert.ToDouble(txtNota4.Text);
+                 
+
+            }
+            catch
+            {
+                MessageBox.Show("SOMENTE NÚMEROS!!");
+                return;
+            }
+
+            string textao = string.Format("\r\nMédia: {0:n}", media);
+
+            txtTextao.Text = textao;
+
+        }
+    }
+}
